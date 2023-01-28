e9fa5234e19d97f373be3e18cb6cf14b5e653540
Flav <110030779+Flavv123@users.noreply.github.com>
2023-01-28 20:36:07 +0000

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,16 @@
+
+function Click_Me(){
+
+    var str = "Click Me";
+    var result = str.fontcolor("green");
+    document.getElementById("Blue_text").innerHTML = result
+
+}
+    
+
+function myFunction(){
+    var sentence = "I am learning";
+    sentence += " A lot about Javascript";
+    result = sentence.fontcolor("green")
+    document.getElementById('Concatenate').innerHTML = result;
+}
