177a6071a2ae8fdfaf008fe43a76d1efef99daef
Flavius <110030779+Flavv123@users.noreply.github.com>
2023-02-14 13:40:13 +0000

    Add files via upload

---
+++ simple Projects/Jquery calculator/jquery/javascript.js
@@ -0,0 +1,66 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+clear(){
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+
+
+}
+
+delete() {
+
+
+}
+
+appendNumber(number){
+    this.currentOperand = number
+
+
+}
+
+chooseOperation(operation){
+
+}
+
+compute() {
+
+
+}
+
+
+
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+
+}
+
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operations]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const currentOperandTextElement = document.querySelector('[data-previous-operand]')
+const previousOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+})
+
+})
+
+
